f16266815c2b45dc05ca2c42d6efbeec08d76df7
aspen8 <alasdair.rae@gmail.com>
2017-09-03 21:56:58 +0100

    commit

---
+++ index.js
@@ -0,0 +1,63 @@
+'use strict';
+var Alexa = require('alexa-sdk');
+
+//=========================================================================================================================================
+//TODO: The items below this comment need your attention.
+//=========================================================================================================================================
+
+//Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.  
+//Make sure to enclose your value in quotes, like this: var APP_ID = "amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1";
+var APP_ID = undefined;
+
+var SKILL_NAME = "Dog's abuse";
+var GET_FACT_MESSAGE = "Here's your fact: ";
+var HELP_MESSAGE = "You can say tell me a space fact, or, you can say exit... What can I help you with?";
+var HELP_REPROMPT = "What can I help you with?";
+var STOP_MESSAGE = "Goodbye!";
+
+//=========================================================================================================================================
+//TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-node-js-fact/data
+//=========================================================================================================================================
+var data = [
+    "Ella's breath is worse than Satan's armpit",
+    "Ella is a dirty wee strumpet",
+    "That dog is a lazy slag",
+    "Ella is a smelly wee cow",
+    "That is the laziest greyhound the world has ever seen",
+    "That dog is a cat in disguise",
+    "I don't believe that dog is an ex racer, she can't run the length of herself",
+];
+
+//=========================================================================================================================================
+//Editing anything below this line might break your skill.  
+//=========================================================================================================================================
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+var handlers = {
+    'LaunchRequest': function () {
+        this.emit('GetNewFactIntent');
+    },
+    'GetNewFactIntent': function () {
+        var factArr = data;
+        var factIndex = Math.floor(Math.random() * factArr.length);
+        var randomFact = factArr[factIndex];
+        var speechOutput = randomFact;
+        this.emit(':tellWithCard', speechOutput, SKILL_NAME, randomFact)
+    },
+    'AMAZON.HelpIntent': function () {
+        var speechOutput = HELP_MESSAGE;
+        var reprompt = HELP_REPROMPT;
+        this.emit(':ask', speechOutput, reprompt);
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit(':tell', STOP_MESSAGE);
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit(':tell', STOP_MESSAGE);
+    }
+};
